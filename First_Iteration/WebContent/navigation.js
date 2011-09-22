33dba52f621f77a3f50f2878b80664e551c4afb2
freecole@gmail.com <freecole@gmail.com>
2011-09-22 21:51:10 +0200

    First commit

---
+++ First_Iteration/WebContent/navigation.js
@@ -0,0 +1,35 @@
+
+    
+    function scrolldown(){
+    var x=400;
+    self.scrollBy(0,x);
+    }
+    
+    function scrollup(){
+        var x=-400;
+        self.scrollBy(0,x);
+        }
+
+  /* 
+    function playautoscroll(){
+    setInterval('scroll()',100);
+   
+    }
+    */
+
+ function goback()
+ {
+	 history.back(); 
+ }
+ 
+ function goforward()
+ {
+	 history.forward(); 
+ }
+  
+ function gohome()
+ {
+	 window.open('http://localhost:8080/First_Iteration/quesOne.html','_self');
+ }
+
+
