e7341555e316d421796adf078bd8b24de8dda60e
Kirti Nathoo <nathoo.kirti@gmail.com>
2011-09-30 10:28:08 +0200

    Changed order of questions. Added second version of survey.

--- WebContent/main.js
+++ WebContent/main.js
@@ -5,8 +5,8 @@ myArray[1] = "What is the average wing-span of bald eagles?";
 myArray[2] = "How many rows of whiskers do cats have?";
 myArray[3] = "How much better is the sence of smell of dogs than humans?"; 
 myArray[4] = "How deep can dolphins swim?";
-myArray[5] = "How many giraffe species are there in total?";
-myArray[6] = "What is a person who makes horse shoes called?";
+myArray[5] = "What is a person who makes horse shoes called?";
+myArray[6] = "How many giraffe species are there in total?";
 myArray[7] = "What is the life expectancy of a shark?";
 myArray[8] = "How much can sloths rotate their heads?";
 
