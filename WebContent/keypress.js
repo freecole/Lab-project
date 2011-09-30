b89be8b61244ad6235adc2997ddc2b50265519ab
Kirti Nathoo <nathoo.kirti@gmail.com>
2011-09-30 13:04:20 +0200

    Now using IIS

--- WebContent/keypress.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-$("button#switch1").keydown(function(){myStartRecognition();});//Note, keydown activates recognition. Stops after some time.
-$("button#switch2").keydown(function(){myStopRecognition();});
-});
-
-
