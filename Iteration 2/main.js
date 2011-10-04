2299efecd2e85a83f00b9eed488b48f537e58d9b
Kirti Nathoo <nathoo.kirti@gmail.com>
2011-10-04 15:16:54 +0200

    - removed the link numbering  - added articles to the link pages :)

--- Iteration 2/main.js
+++ Iteration 2/main.js
@@ -1,17 +1,17 @@
-// Main .js file from which data is extracted
+function date_time()
+{
+	document.write("Last updated: ");	
+	// Date
+	var today_date = new Date();
+	var day = today_date.getDate();
+	var month = today_date.getMonth()+1;
+	var year = today_date.getFullYear();
+	document.writeln(day + "/" + month + "/" + year);
 	
-document.write("Last updated: ");
-// Date
-var today_date = new Date();
-var day = today_date.getDate();
-var month = today_date.getMonth()+1;
-var year = today_date.getFullYear();
-document.writeln(day + "/" + month + "/" + year);
- //document.write(" ");
-	
-// Time
-var hour = today_date.getHours();
-var mins = today_date.getMinutes();
-var sec = today_date.getSeconds();
-document.writeln(hour + ":" + mins + ":" + sec);
+    // Time
+    var hour = today_date.getHours();
+    var mins = today_date.getMinutes();
+    var sec = today_date.getSeconds();
+    document.writeln(hour + ":" + mins + ":" + sec);
+}
 
