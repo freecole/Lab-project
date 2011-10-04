d3f719751be11ba8caf6684713a0396d6ef40d3c
Kirti Nathoo <nathoo.kirti@gmail.com>
2011-10-04 11:19:35 +0200

    - bulked up iteration 1 - put files into folders

--- Iteration 1/main.js
+++ Iteration 1/main.js
@@ -1,14 +1,14 @@
 // array to store questions
 var myArray = new Array();
-var index=0;
+// questions for numerical and spoken link referencing
 myArray[1] = "What is the average wing-span of bald eagles?";
 myArray[2] = "How many rows of whiskers do cats have?";
 myArray[3] = "How much better is the sence of smell of dogs than humans?"; 
 myArray[4] = "How deep can dolphins swim?";
 myArray[5] = "What is a person who makes horse shoes called?";
+// questions for visual (pop up, highlight) and verbal feedback
 myArray[6] = "How many giraffe species are there in total?";
 myArray[7] = "What is the life expectancy of a shark?";
-myArray[8] = "How much can sloths rotate their heads?";
 
 function date_time()
 {
